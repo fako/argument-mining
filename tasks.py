04c0cc9189d202dcd7678044f12d000961f906df
Fako Berkers <email@fakoberkers.nl>
2023-09-24 18:59:35 +0200

    Refactors the analysis module

--- tasks.py
+++ tasks.py
@@ -2,7 +2,8 @@ from invoke import Collection
 from jinja2 import Environment, PackageLoader
 
 from load import load_stance_classification_dataset, unpack_discourse_dataset
-from analysis import analyse_stance_classification_dataset, analyse_chatgpt_stance_classification, cluster_collection
+from analysis.stance_classification import (analyse_stance_classification_dataset, analyse_chatgpt_stance_classification,
+                                            cluster_collection)
 from sampling import sample_stance_classification_dataset, sample_discourse_dataset
 from fetch import fetch_collection
 from backup import backup_stance_classification_output, sync_discourse_output
