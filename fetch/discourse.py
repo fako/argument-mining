c7b1c67917df51a8cee6691bb13ead76b65ade32
Fako Berkers <email@fakoberkers.nl>
2023-10-22 20:50:29 +0200

    Adds fetch embeddings command for discourse

--- fetch/discourse.py
+++ fetch/discourse.py
@@ -2,6 +2,7 @@ from invoke import task
 
 from prompts.chatgpt import ChatGPTPrompt
 from load.iterators import DiscourseRecordIterator
+from embeddings.chatgpt import ChatGPTEmbeddings
 
 
 MODEL = "gpt-4-0613"
@@ -23,3 +24,31 @@ def assess_discourse(ctx, scope, discourse, dry_run=False, limit=None):
     )
     for identifier, record, aspects in post_iterator:
         chatgpt.fetch(identifier, record["text"], dry_run=dry_run)
+
+
+@task()
+def embeddings_discourse(ctx, scope, discourse, dry_run=False, limit=None):
+    limit = int(limit) if limit is not None else None
+    chatgpt = ChatGPTEmbeddings(ctx.config, "claims")
+    post_iterator = DiscourseRecordIterator(
+        project=discourse,
+        output_directory=ctx.config.directories.output,
+        scope=scope,
+        max_text_length=ChatGPTPrompt.text_length_limit - 200,
+        limit=limit,
+        prompts={"assessment": ChatGPTPrompt(ctx.config, "assessment")},
+        cut_off_text=True
+    )
+    for identifier, record, prompts in post_iterator:
+        assessment = prompts.get("assessment", None)
+        if not assessment:
+            print("Missing assessment prompt:", identifier)
+            continue
+
+        for premise in assessment.get("premises", []):
+            cache_key = chatgpt.get_cache_key(identifier, premise)
+            chatgpt.fetch(cache_key, premise, dry_run=dry_run)
+        conclusion = assessment.get("conclusion", None)
+        if conclusion:
+            cache_key = chatgpt.get_cache_key(identifier, conclusion)
+            chatgpt.fetch(cache_key, conclusion)
